84e7e664da2684e7c78d3545a43ec47b67491ab9
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-28 21:25:18 -0200

    Adição da classe js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('Erislandio')
+// alert('Erislandio' *
+
+// ! classes es6
+
+class TodoList {
+    constructor() {
+        this.todos = []
+    }
+
+    addTodo() {
+        this.todos.push("novo todo")
+        console.log(this.todos)
+    }
+}
+
+const minhaList = new TodoList();
+
+document.getElementById('btn').onclick = function() {
+    minhaList.addTodo();
+}
